9a24ce856c8da8bd066d56e2b7901b443e7d2420
EmaanHumayun <emaanhumayun23072@gmail.com>
2026-03-16 19:32:17 +0500

    added lab 04

---
+++ lab_04.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int marks;
+    // cin >> marks;
+    // if(marks>=50)
+    // cout<< "pass";
+    // else
+    // cout<< "fail";
+    //task 2
+    // 
+    //task 3
+    // int a,b;
+    // cin>> a>>b;
+    // if(a>b)
+    // cout<<"a is larger";
+    // else
+    // cout<< "b is larger";
+    // task 4
+    // for(int i=1;i<=20;i++)
+    // cout<<i<<endl;
+    //task 5
+    // int i=1;
+    // while(i<=20){
+    //     cout<< i<<endl;
+    //     i++
+    // }
+   for(int i=1;i<=5;i++) {
+
+     for(int j=1;j<=i;j++){
+        cout<<"*";
+     }
+     cout<<endl;
+   }
+  
+}
